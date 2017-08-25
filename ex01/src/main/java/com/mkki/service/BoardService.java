7568c4e66473fb5f6b7c2ce00363842cb50694f7
Mkki <gosorn1@naver.com>
2017-08-26 01:00:14 +0900

    [2017/08/26 01:00:00] Commit

---
+++ ex01/src/main/java/com/mkki/service/BoardService.java
@@ -0,0 +1,18 @@
+package com.mkki.service;
+
+import java.util.List;
+
+import com.mkki.domain.BoardVO;
+
+public interface BoardService {
+	public void regist(BoardVO board) throws Exception;
+	
+	public BoardVO read(Integer bno) throws Exception;
+	
+	public void modify(BoardVO board) throws Exception;
+	
+	public void remove(Integer bno) throws Exception;
+	
+	public List<BoardVO> listAll() throws Exception;
+	
+}
---
+++ ex01/src/test/java/com/mkki/test/BoardDAOTest.java
@@ -0,0 +1,20 @@
+package com.mkki.test;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.mkki.domain.BoardVO;
+import com.mkki.persistence.BoardDAO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+public class BoardDAOTest {
+
+	@Inject
+	private BoardDAO dao;
+
+}
